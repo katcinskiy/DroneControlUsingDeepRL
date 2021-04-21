f88bd74cd8568326c44157d764e02e761b4c9638
Alexander <alexander.yukhimchuk@yandex.by>
2021-04-21 14:55:54 +0300

    reinforce, a2c

---
+++ a2c.py
@@ -0,0 +1,113 @@
+import torch
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_episodes = 1000
+n_steps = 10000
+
+env = gym.make("CartPole-v1")
+env._max_episode_steps = 10000
+state = env.reset()
+n_actions = env.action_space.n
+n_states = env.observation_space.shape[0]
+
+
+class Actor(torch.nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(in_dim, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, out_dim),
+            torch.nn.Softmax()
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+    def get_action(self, x):
+        probs = self.forward(x)
+        dist = torch.distributions.Categorical(probs)
+        sample = dist.sample()
+        return sample.item(), torch.log(probs[sample])
+
+
+class Critic(torch.nn.Module):
+    def __init__(self, in_dim):
+        super().__init__()
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(in_dim, 512),
+            torch.nn.ReLU(),
+            torch.nn.Linear(512, 256),
+            torch.nn.ReLU(),
+            torch.nn.Linear(256, 128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(128, 1),
+        )
+
+    def forward(self, x):
+        return self.layers(x)
+
+
+def update(rewards, log_probs, values, actor_optimizer, critic_optimizer):
+    actor_optimizer.zero_grad()
+    critic_optimizer.zero_grad()
+    q_values = torch.zeros_like(log_probs)
+    q_val = 0
+    for i in reversed(range(len(rewards))):
+        q_val = 0.9 * q_val + rewards[i]
+        q_values[i] = q_val
+    advantage = q_values - values
+    actor_loss = (-log_probs * advantage.detach()).mean()
+    critic_loss = 0.5 * advantage.pow(2).mean()
+    actor_loss.backward()
+    critic_loss.backward()
+    actor_optimizer.step()
+    critic_optimizer.step()
+
+actor = Actor(n_states, n_actions)
+critic = Critic(n_states)
+actor_optimizer = torch.optim.Adam(actor.parameters(), lr=3e-4)
+critic_optimizer = torch.optim.Adam(critic.parameters(), lr=3e-4)
+
+count_steps = []
+
+for episode in range(n_episodes):
+    rewards = []
+    prev_states = []
+    states = []
+    actions = []
+    log_probs = []
+    values = []
+    state = env.reset()
+    count = 0
+    for step in range(n_steps):
+        count += 1
+        state = torch.FloatTensor(state)
+        prev_states.append(state)
+        a, log_prob = actor.get_action(state)
+        value = critic.forward(state)
+        values.append(value[0])
+        state, reward, done, _ = env.step(a)
+        env.render()
+        rewards.append(reward)
+        states.append(state)
+        actions.append(a)
+        log_probs.append(log_prob)
+        if done:
+            break
+    count_steps.append(count)
+    print('episode {}, count {}'.format(episode, count))
+    update(rewards, torch.stack(log_probs), torch.stack(values), actor_optimizer, critic_optimizer)
+
+exp_count_steps = [count_steps[0]]
+for i in range(1, len(count_steps)):
+    exp_count_steps.append(0.4 * exp_count_steps[i - 1] + 0.6 * count_steps[i])
+plt.plot(count_steps)
+plt.plot(exp_count_steps)
+plt.show()
---
+++ test.py
@@ -0,0 +1,112 @@
+import gym
+import torch
+import numpy as np
+from torch.autograd import Variable
+import matplotlib.pyplot as plt
+
+if torch.cuda.is_available():
+  dev = "cuda:0"
+else:
+  dev = "cpu"
+
+save_model = True
+
+env = gym.make('CartPole-v1')
+state = env.reset()
+n_states = env.observation_space.shape[0]
+n_actions = env.action_space.n
+n_episodes = 1000
+max_steps = 1000
+GAMMA = 0.9
+
+
+class Reinforce(torch.nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super().__init__()
+        self.num_actions = out_dim
+        self.linear1 = torch.nn.Linear(in_dim, 256)
+        self.linear2 = torch.nn.Linear(256, 64)
+        self.linear3 = torch.nn.Linear(64, out_dim)
+
+    def forward(self, x):
+        x = self.linear1(x).clamp(min=0)
+        x = self.linear2(x).clamp(min=0)
+        x = self.linear3(x)
+        return torch.nn.functional.softmax(x, dim=1)
+
+    def get_action(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        probs = self.forward(Variable(state))
+        highest_prob_action = np.random.choice(self.num_actions, p=np.squeeze(probs.detach().numpy()))
+        log_prob = torch.log(probs.squeeze(0)[highest_prob_action])
+        return highest_prob_action, log_prob
+
+
+def update_policy(optimizer, rewards, log_probs):
+    discounted_rewards = []
+
+    for t in range(len(rewards)):
+        Gt = 0
+        pw = 0
+        for r in rewards[t:]:
+            Gt = Gt + GAMMA ** pw * r
+            pw = pw + 1
+        discounted_rewards.append(Gt)
+
+    discounted_rewards = torch.tensor(discounted_rewards)
+    discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (
+                discounted_rewards.std() + 1e-9)  # normalize discounted rewards
+
+    policy_gradient = []
+    for log_prob, Gt in zip(log_probs, discounted_rewards):
+        policy_gradient.append(-log_prob * Gt)
+
+    optimizer.zero_grad()
+    policy_gradient = torch.stack(policy_gradient).sum()
+    policy_gradient.backward()
+    optimizer.step()
+
+
+model = Reinforce(n_states, n_actions)
+# model.load_state_dict(torch.load("reinforce"))
+model.to(dev)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
+rewards_size = []
+
+for episode in range(n_episodes):
+    state = env.reset()
+    print(episode)
+    rewards = []
+    actions = []
+    states = []
+    log_probs = []
+    for step in range(max_steps):
+        highest_prob_action, log_prob = model.get_action(state)
+        state, reward, done, _ = env.step(highest_prob_action)
+        env.render()
+
+        states.append(state)
+        actions.append(highest_prob_action)
+        rewards.append(reward)
+        log_probs.append(log_prob)
+        if done:
+            print('steps - %f', step)
+            break
+
+    update_policy(optimizer, rewards, log_probs)
+    rewards_size.append(len(rewards))
+    torch.save(model.state_dict(), 'reinforce')
+
+plt.plot(rewards_size)
+plt.show()
+# alpha = 1
+# while alpha != 0:
+#     smoothed = []
+#     smoothed.append(rewards_size[0])
+#     for i in range(1, len(rewards_size)):
+#         smoothed.append(alpha * rewards_size[i] + (1 - alpha) * smoothed[i - 1])
+#     plt.plot(smoothed)
+#     plt.legend(alpha)
+#     plt.show()
+#     alpha -= 0.05
